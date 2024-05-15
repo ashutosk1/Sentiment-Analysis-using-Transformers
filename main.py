663e5baaf6c470c9c9b6743ca5a8b25fb65fe3a6
ashutosk1 <ashutosh.k0342@gmail.com>
2024-05-15 18:06:56 +0530

    Add Transfer Learning Pipeline on Pre-trained BERT model

--- main.py
+++ main.py
@@ -1,34 +1,30 @@
 import constants 
-from model import encoder, split_train_test_data
+from model import BERTForClassification, load_pretrained_model, load_pretrained_tokenizer, train
 from classifier import TextClassifier
 from preprocess import preprocess_data
+
+from pathlib import Path
 
 import pickle
 
 if __name__ == "__main__":
-    # Data clean-up and pre-process
-    #corpus_preprocessed, sentiment = preprocess_data()
-
-    # with open("corpus.pickle", "wb") as f:
-    #     pickle.dump(corpus_preprocessed, f)
-    #     print("Corpus saved as pickle file")
     
-    # with open("label.pickle", "wb") as f:
-    #     pickle.dump(sentiment, f)
-    #     print("Label saved as pickle file")
-
     try:
         with open("corpus.pickle", "rb") as f:
-            corpus_preprocessed=pickle.load(f)
+            corpus=pickle.load(f)
             print("Loaded corpus successfully")
 
         with open("label.pickle", "rb") as f:
-            sentiment= pickle.load(f)
+            labels= pickle.load(f)
             print("Loaded labels successfully")
     except:
         print("Some Error encountered.")
 
-    X_train, X_test, y_train, y_test = split_train_test_data(corpus_preprocessed[:1000], sentiment[:1000])
-    encodings, _= encoder(X_train, 'distilbert-base-uncased')
- 
+    # Classify and Train
+    classifier = BERTForClassification()
+    history = train(classifier, corpus[:constants.NUM_EXAMPLES], labels[:constants.NUM_EXAMPLES])
+
+    
+
+    
     
--- model.py
+++ model.py
@@ -5,8 +5,8 @@ from sklearn.metrics import precision_score
 from tensorflow.keras.optimizers import Adam, SGD
 
 
-import transformers
-from transformers import DistilBertTokenizer, TFDistilBertModel
+from transformers import TFAutoModel, AutoTokenizer
+
 
 import logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
@@ -14,79 +14,77 @@ import tqdm
 
 import constants
 
-def split_train_test_data(corpus, labels):
-    """ Splits the Training and Test Data in the proportion of the 'test_size'
-        Returns:
-            X_train, y_train, X_test, y_test
-    """
-    X_train, X_test, y_train, y_test= train_test_split(corpus, labels, test_size = constants.TEST_SIZE, random_state = 42)
-    y_train = np.array(y_train, dtype="float32").reshape(-1,1)
-    y_test = np.array(y_test, dtype="float32").reshape(-1,1)
-    logging.info("Size of Training Data: %d, Size of Test Data: %d", len(X_train), len(X_test))
-    
-    return X_train, y_train, X_test, y_test
 
 
-# def encoder(data, tokenizer, max_length):
-#     """
-#     """
-#     input_ids=[]
-#     attention_masks=[]
+class BERTForClassification(tf.keras.Model):
 
-#     for i in range(len(data)):
-#         encoded = tokenizer.encode_plus(
-#                                         data[i], 
-#                                         truncation=True,
-#                                         max_length=constants.SEQ_LENGTH, 
-#                                         padding="max_length",
-#                                         add_special_tokens=True
-#                                         )
-#         input_ids.append(encoded["input_ids"])
-#         attention_masks.append(encoded["attention_mask"])
-    
-#     return np.array(input_ids), np.array(attention_masks)
-    
+    def __init__(self):
+        super().__init__()
+        self.bert = TFAutoModel.from_pretrained(constants.MODEL_NAME)
+        self.num_classes = constants.NUM_CLASSES
+        self.seq_length = constants.SEQ_LENGTH
+
+        # Define the classification head
+        self.dense1 = tf.keras.layers.Dense(128, activation='relu')
+        self.dropout1 = tf.keras.layers.Dropout(0.5)
+        self.dense2 = tf.keras.layers.Dense(64, activation='relu')
+        self.dropout2 = tf.keras.layers.Dropout(0.5)
+        self.dense3 = tf.keras.layers.Dense(8, activation='relu')
+        self.dropout3 = tf.keras.layers.Dropout(0.5)
+        self.output_layer = tf.keras.layers.Dense(self.num_classes, activation='softmax')
 
 
-def encoder(data, model_name):
-    """
-    """
-    tokenizer = DistilBertTokenizer.from_pretrained(model_name, force_download=True)
-    input_ids=[]
-    attention_masks=[]
+    def call(self, inputs):
 
-    for i in (range(len(data))):
-        encoded= tokenizer(data, 
-                            truncation=True,                
-                            max_length=constants.SEQ_LENGTH, 
-                            padding="max_length",
-                            add_special_tokens=True
-                            )
-        input_ids.append(encoded["input_ids"])
-        attention_masks.append(encoded["attention_mask"])
+        x = self.bert(inputs)[1]  # Extract the output from the second layer (sequence embedding)
 
-        if ((i+1)%100==0): print(f"PROGRESS: {i+1}")
-
-    return np.array(input_ids), np.array(attention_masks)
+        # Classification head for multi-class classification
+        x = self.dense1(x)
+        x = self.dropout1(x)
+        x = self.dense2(x)
+        x = self.dropout2(x)
+        x = self.dense3(x)
+        x = self.dropout3(x)
+        outputs = self.output_layer(x)
+        return outputs
 
 
+def tokenize(tokenizer, batch_data, seq_length):
+  encoded_text = tokenizer(batch_data, padding=True, max_length=seq_length, truncation=True, return_tensors='tf')
+  return [
+          encoded_text['input_ids'],
+          encoded_text['attention_mask'],
+          encoded_text['token_type_ids']
+          ]
 
 
-def build_model(pre_trained_model, input_length):
-    """
-    """
-    input_ids= tf.keras.Input(shape=(input_length,), dtype='int32')
-    attention_masks = tf.keras.Input(shape=(input_length,), dtype='int32')
+def one_hot_label(label):
+    return tf.one_hot(label, depth=2)
 
-    x_layer = pre_trained_model([input_ids, attention_masks])[-1]
+def load_pretrained_model():
+    return TFAutoModel.from_pretrained(constants.MODEL_NAME)
 
-    if len(x_layer.get_shape())> 2: x_layer = tf.squeeze(x_layer[:, -1:, :], axis=1)
+def load_pretrained_tokenizer():
+    return AutoTokenizer.from_pretrained(constants.MODEL_NAME)
 
-    x_layer = tf.keras.layers.Dense(32, activation='relu')(x_layer)
-    x_layer = tf.keras.layers.Dropout(0.2)(x_layer)
-    x_layer = tf.keras.layers.Dense(1, activation='sigmoid')(x_layer)
 
-    model = tf.keras.models.Model(inputs= ([input_ids, attention_masks]) ,outputs = x_layer)
-    model.compile(Adam(lr=6e-6), loss='binary_crossentropy', metrics=['accuracy'])
-    return model
+def train(model, tweets, labels):
+    # Train/Test Split
+    X_train, X_test, y_train, y_test = train_test_split(tweets, labels, test_size=constants.TEST_SIZE, random_state=42, shuffle=True)
 
+    tokenizer = load_pretrained_tokenizer()
+    X_train_encoded = tokenize(tokenizer, X_train, constants.SEQ_LENGTH)
+    X_test_encoded = tokenize(tokenizer, X_test, constants.SEQ_LENGTH)
+
+    y_train_hot = one_hot_label(y_train)
+    y_test_hot = one_hot_label(y_test)
+
+    model.summary()
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=3)
+    history = model.fit(x=X_train_encoded, y=y_train_hot,
+                      validation_data=(X_test_encoded, y_test_hot),
+                      epochs=constants.EPOCHS,
+                      callbacks=[early_stopping],
+                      batch_size=constants.BATCH_SIZE)
+    return history
